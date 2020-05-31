1877ba161cde9c3f6cad584b8f85b28f76f675d2
Olufemi Afolabi <femiafolabidiipo@gmail.com>
2020-05-30 23:20:40 -0500

    vue quiz

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<Header :numCorrect="numCorrect" :numTotal="numTotal"></Header>
+		<b-container class="bv-example-row">
+			<b-row>
+				<b-col sm="6" offset="3">
+					<QuestionBox
+						v-if="questions.length"
+						:currentQuestion="questions[index]"
+						:next="next"
+						:increment="increment"
+					/>
+				</b-col>
+			</b-row>
+		</b-container>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import QuestionBox from "./components/QuestionBox.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		Header,
+		QuestionBox,
+	},
+	data() {
+		return {
+			questions: [],
+			index: 0,
+			numCorrect: 0,
+			numTotal: 0,
+		};
+	},
+	mounted: function() {
+		fetch("https://opentdb.com/api.php?amount=10&category=19", {
+			method: "get",
+		})
+			.then((res) => {
+				return res.json();
+			})
+			.then((data) => {
+				console.log(data);
+				this.questions = data.results;
+			});
+	},
+	methods: {
+		next() {
+			if (this.index < this.questions.length - 1) {
+				this.index++;
+			}
+		},
+		increment(isCorrect) {
+			if (isCorrect) {
+				this.numCorrect += 1;
+			}
+			this.numTotal += 1;
+		},
+	},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<b-nav tabs>
+			<b-nav-item disabled>Fancy QuixApp</b-nav-item>
+			<b-nav-item disabled>Counter: {{ numCorrect }}/{{ numTotal }}</b-nav-item>
+		</b-nav>
+	</div>
+</template>
+
+<script>
+export default {
+	props: { numCorrect: Number, numTotal: Number },
+};
+</script>
